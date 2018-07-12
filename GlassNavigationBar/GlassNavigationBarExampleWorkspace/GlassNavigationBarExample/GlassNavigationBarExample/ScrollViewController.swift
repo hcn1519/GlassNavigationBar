13bba053da8593838732196fac7ee68318eb4963
Seong ho Hong <grohong76@gmail.com>
2018-07-12 13:39:18 +0900

    [config]: set scrollview example

---
+++ GlassNavigationBar/GlassNavigationBarExampleWorkspace/GlassNavigationBarExample/GlassNavigationBarExample/ScrollViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ScrollViewController.swift
+//  GlassNavigationBarExample
+//
+//  Created by Seong ho Hong on 2018. 7. 12..
+//  Copyright © 2018년 Seong ho Hong. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
