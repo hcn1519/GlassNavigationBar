998d86baac7c6ba50cb2d633b9dbf38a4c7cc097
Seong ho Hong <grohong76@gmail.com>
2018-07-14 17:32:01 +0900

    [feature]: set collectionview example

---
+++ GlassNavigationBar/GlassNavigationBarExampleWorkspace/GlassNavigationBarExample/GlassNavigationBarExample/CollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewController.swift
+//  GlassNavigationBarExample
+//
+//  Created by Seong ho Hong on 2018. 7. 13..
+//  Copyright © 2018년 Seong ho Hong. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
